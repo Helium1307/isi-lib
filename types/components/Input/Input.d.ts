f54df5dbaa7348eb16df3d31cf7a93eef0ba5692
Helium1307 <heliofilho1307@hotmail.com>
2022-03-27 02:07:11 -0300

    tirar o teste

--- types/components/Input/Input.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export declare type InputProps = {
-    type: string;
-};
-declare const Input: (props: InputProps) => JSX.Element;
-export default Input;
--- types/components/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from "./Button";
-export type { ButtonProps } from "./Button/Button";
-export { default as Input } from "./Input";
-export type { InputProps } from "./Input/Input";
