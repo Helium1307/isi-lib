0bea01cc133cd6ce73fcc666f18c11e37cca86f7
Helium1307 <heliofilho1307@hotmail.com>
2022-03-27 02:06:08 -0300

    teste

---
+++ types/components/Button/Button.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+export declare type ButtonProps = {
+    label: string;
+};
+declare const Button: (props: ButtonProps) => JSX.Element;
+export default Button;
---
+++ types/components/Input/Input.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+export declare type InputProps = {
+    type: string;
+};
+declare const Input: (props: InputProps) => JSX.Element;
+export default Input;
